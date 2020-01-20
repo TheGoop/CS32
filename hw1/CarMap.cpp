7e2a676ea3ab32c34300577b7ebf645c81956615
Akshay Gupta <akshaygupta.us@gmail.com>
2020-01-20 02:43:20 -0800

    Add files via upload

---
+++ hw1/CarMap.cpp
@@ -0,0 +1,9 @@
+//
+//  CarMap.cpp
+//  map
+//
+//  Created by Akshay Gupta on 1/20/20.
+//  Copyright © 2020 hw1. All rights reserved.
+//
+
+#include "CarMap.h"
---
+++ hw1/Map.h
@@ -0,0 +1,96 @@
+//
+//  Map.h
+//  map
+//
+//  Created by Akshay Gupta on 1/19/20.
+//  Copyright © 2020 hw1. All rights reserved.
+//
+
+#ifndef Map_h
+#define Map_h
+
+#include <string>
+
+using KeyType = int;
+using ValueType = std::string;
+const int DEFAULT_MAX_ITEMS = 240;
+
+class Map
+{
+  public:
+    Map(): m_size(0) {};   // Create an empty map (i.e., one with no key/value pairs)
+
+    //DONE
+    bool empty() const;  // Return true if the map is empty, otherwise false.
+
+    //DONE
+    int size() const;    // Return the number of key/value pairs in the map.
+
+    //DONE
+    bool insert(const KeyType& key, const ValueType& value);
+      // If key is not equal to any key currently in the map, and if the
+      // key/value pair can be added to the map, then do so and return true.
+    // Otherwise, m*ake no change to the map and return false (indicating
+      // that either the key is already in the map, or the map has a fixed
+      // capacity and is full).
+
+    //DONE
+    bool update(const KeyType& key, const ValueType& value);
+      // If key is equal to a key currently in the map, then make that key no
+      // longer map to the value it currently maps to, but instead map to
+      // the value of the second parameter; return true in this case.
+      // Otherwise, make no change to the map and return false.
+
+    //DONE
+    bool insertOrUpdate(const KeyType& key, const ValueType& value);
+      // If key is equal to a key currently in the map, then make that key no
+      // longer map to the value that it currently maps to, but instead map to
+      // the value of the second parameter; return true in this case.
+      // If key is not equal to any key currently in the map and if the
+      // key/value pair can be added to the map, then do so and return true.
+      // Otherwise, make no change to the map and return false (indicating
+      // that the key is not already in the map and the map has a fixed
+      // capacity and is full).
+
+    //DONE
+    bool erase(const KeyType& key);
+      // If key is equal to a key currently in the map, remove the key/value
+      // pair with that key from the map and return true.  Otherwise, make
+      // no change to the map and return false.
+     
+    //DONE
+    bool contains(const KeyType& key) const;
+      // Return true if key is equal to a key currently in the map, otherwise
+      // false.
+     
+    //DONE
+    bool get(const KeyType& key, ValueType& value) const;
+      // If key is equal to a key currently in the map, set value to the
+      // value in the map which that key maps to, and return true.  Otherwise,
+      // make no change to the value parameter of this function and return
+      // false.
+     
+    //DONE
+    bool get(int i, KeyType& key, ValueType& value) const;
+      // If 0 <= i < size(), copy into the key and value parameters the
+      // key and value of one of the key/value pairs in the map and return
+      // true.  Otherwise, leave the key and value parameters unchanged and
+      // return false.  (See below for details about this function.)
+    //DONE
+    void swap(Map& other);
+      // Exchange the contents of this map with the other one.
+    
+    void printAll();
+    
+    private:
+        struct Dict
+        {
+            KeyType key;
+            ValueType val;
+        };
+        int m_size;
+        Dict m_array[DEFAULT_MAX_ITEMS];
+};
+
+
+#endif /* Map_h */
